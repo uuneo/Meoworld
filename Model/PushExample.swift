ca298ab2993c2068257cc52a1e9450cb4e71dfe3
“twoown” <admin@twown.com>
2024-09-02 12:05:25 +0800

    change example bugs

--- Model/PushExample.swift
+++ Model/PushExample.swift
@@ -11,5 +11,5 @@ struct PushExample:Identifiable {
     var id = UUID().uuidString
     var header,footer,title,params:String
     
-    static let datas:[PushExample] =  Array(0...13).map({ PushExample(header: NSLocalizedString("pushExampleHeader\($0)",comment: ""), footer: NSLocalizedString("pushExampleFooter\($0)",comment: ""), title: NSLocalizedString("pushExampleTitle\($0)",comment: ""),params: NSLocalizedString("pushExampleParams\($0)",comment: ""))})
+    static let datas:[PushExample] =  Array(0...12).map({ PushExample(header: NSLocalizedString("pushExampleHeader\($0)",comment: ""), footer: NSLocalizedString("pushExampleFooter\($0)",comment: ""), title: NSLocalizedString("pushExampleTitle\($0)",comment: ""),params: NSLocalizedString("pushExampleParams\($0)",comment: ""))})
 }
